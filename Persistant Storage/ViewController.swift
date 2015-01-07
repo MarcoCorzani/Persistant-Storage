b916579cc579a3837dd70a8c1b9df94a0246ac57
Marco <Marco@Marcos-iMac.fritz.box>
2015-01-07 20:32:14 +0100

    Anfang

--- Persistant Storage/ViewController.swift
+++ Persistant Storage/ViewController.swift
@@ -8,18 +8,39 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    
+    @IBOutlet weak var myTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+    
+//    NSUserDefaults.standardUserDefaults().setObject("Marco", forKey: "myName")
+//    
+//    NSUserDefaults.standardUserDefaults().synchronize()
+//    
+//    println(NSUserDefaults.standardUserDefaults().objectForKey("myName")!)
+    
+//      NSUserDefaults.standardUserDefaults().removeObjectForKey("myName")
+    
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    
+    
+    override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
+        self.view.endEditing(true)
     }
 
-
+    
+    func textFieldShouldReturn(textField: UITextField!) -> Bool {
+        
+        textField.resignFirstResponder()
+        
+        return true
+    }
 }
 
